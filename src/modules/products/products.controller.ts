4938d9672b850b4ed16e119f14412c7852ee124f
ductiens <carina.ndt@gmail.com>
2026-07-03 17:17:24 +0700

    feat: implement products CRUD operations and add DTO for product creation

--- src/modules/products/products.controller.ts
+++ src/modules/products/products.controller.ts
@@ -1,7 +1,46 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import Product from 'src/entities/Product';
 import { ProductsService } from './products.service';
 
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
+
+  @Get()
+  findAll() {
+    return this.productsService.findAll();
+  }
+
+  @Post()
+  create(@Body() productData: Partial<Product>) {
+    return this.productsService.create(productData);
+  }
+
+  @Patch(':id')
+  update(@Body() productData: Partial<Product>, @Param('id') id: number) {
+    return this.productsService.update(id, productData);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: number) {
+    return this.productsService.remove(id);
+  }
+
+  // @Get(':id')
+  // findOne(@Param('id') id: string) {
+  //   return this.productsService.findOne(id);
+  // }
+
+  // @Delete(':id')
+  // remove(@Param('id') id: string) {
+  //   return this.productsService.remove(id);
+  // }
 }
--- src/modules/products/products.service.ts
+++ src/modules/products/products.service.ts
@@ -1,4 +1,39 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import Product from 'src/entities/Product';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class ProductsService {}
+export class ProductsService {
+  constructor(
+    @InjectRepository(Product)
+    private readonly productsRepository: Repository<Product>,
+  ) {}
+
+  findAll() {
+    return this.productsRepository.find();
+  }
+
+  create(productData: Partial<Product>) {
+    const product = this.productsRepository.create(productData);
+    product.created_at = new Date();
+    product.updated_at = new Date();
+    return this.productsRepository.save(product);
+  }
+
+  async update(id: number, productData: Partial<Product>) {
+    productData.updated_at = new Date();
+    await this.productsRepository.update(id, productData);
+    return this.productsRepository.findOneBy({ id });
+  }
+
+  // findOne(id: string) {
+  //   return this.productsRepository.findOne(id);
+  // }
+
+  async remove(id: number) {
+    const product = await this.productsRepository.findOneBy({ id });
+    await this.productsRepository.delete(id);
+    return product;
+  }
+}
